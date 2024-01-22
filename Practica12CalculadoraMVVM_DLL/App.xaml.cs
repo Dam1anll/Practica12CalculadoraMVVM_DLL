3e84f5e659abd66ae722d20989cd8a41db026cf4
Dam1anll <damiianll20@gmail.com>
2024-01-22 09:18:49 -0700

    Agregar archivos de proyecto.

---
+++ Practica12CalculadoraMVVM_DLL/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Practica12CalculadoraMVVM_DLL.View;
+
+namespace Practica12CalculadoraMVVM_DLL
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Calculadora();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Practica12CalculadoraMVVM_DLL/ViewModel/VMCalculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.Linq;
+using Practica12CalculadoraMVVM_DLL.View;
+using System.Threading.Tasks;
+using Practica12CalculadoraMVVM_DLL.ViewModel;
+
+namespace Practica12CalculadoraMVVM_DLL.ViewModel
+{
+    public class VMCalculadora : BaseViewModel
+    {
+      
+    }
+}
+
